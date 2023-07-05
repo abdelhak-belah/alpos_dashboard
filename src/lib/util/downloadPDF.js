66ff1efc1fce9fe4281e531559a2aff362a4a7ef
Abdelhak <46165987+abdelhak-belah@users.noreply.github.com>
2023-07-05 09:55:39 +0100

    Update downloadPDF.js

--- src/lib/util/downloadPDF.js
+++ src/lib/util/downloadPDF.js
@@ -1,8 +1,9 @@
 import pdfMake from 'pdfmake/build/pdfmake';
-import pdfFonts from 'pdfmake/build/vfs_fonts';
+import { vfs } from 'pdfmake/build/pdfmake';
 import {createPDF} from "$lib/util/createPDF.js";
 
 export function downloadPDF(scan) {
+    const pdfFonts = vfs.pdfMake.vfs;
     pdfMake.vfs = pdfFonts.pdfMake.vfs;
     var docDefinition = createPDF(scan);
     pdfMake.createPdf(docDefinition).download('scan_rapport.pdf');
